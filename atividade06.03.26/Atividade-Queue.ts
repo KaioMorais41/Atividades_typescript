ba992f52d0e63100e1ca2873b32e4dda6d0bf6b1
KaioMorais41 <kaiomorais2903@gmail.com>
2026-03-08 16:16:18 -0300

    Feat: Atividade 06.03.2026

--- atividade06.03.26/Atividade-Queue.ts
+++ atividade06.03.26/Atividade-Queue.ts
@@ -1,5 +1,61 @@
 // Fila
 
 import { Queue } from "./Queue";
+import readlinesync = require("readline-sync");
 
-const fila = new Queue<number>() // o tipo number/string sera decidido na hora de usar
+const fila = new Queue<string>();
+let opcao: number;
+
+do {
+
+    console.log("\n-------------------------------------------");
+    console.log("1 - Adicionar Cliente na Fila");
+    console.log("2 - Listar todos os Clientes");
+    console.log("3 - Retirar Cliente da Fila");
+    console.log("0 - Sair");
+    console.log("-------------------------------------------");
+
+    opcao = readlinesync.questionInt("Entre com a opcao desejada: ");
+
+    switch(opcao){
+
+        case 1:
+            let cliente = readlinesync.question("Digite o nome do Cliente: ");
+            fila.enqueue(cliente);
+            console.log("\nFila: \n");
+            fila.printQueue();
+            console.log("\nCliente Adicionado!");
+
+        break;
+
+        case 2:
+            console.log("\nLista de Clientes na Fila: \n");
+            fila.printQueue();
+        break;
+
+        case 3:
+        if (fila.isEmpty()) {
+        console.log("\nA fila esta vazia!");
+        } else {
+        let clienteChamado = fila.dequeue();
+
+        console.log("\nFila: \n");
+
+        fila.printQueue();
+
+        console.log(`\nO Cliente ${clienteChamado} foi chamado!`);
+
+        }
+        
+        break;
+
+        case 0:
+            console.log("\nPrograma Finalizado!");
+        break;
+
+        default:
+            console.log("\nOpcao invalida!");
+        break;
+    }
+
+}while(opcao !== 0);
--- atividade06.03.26/Atividade-Stack.ts
+++ atividade06.03.26/Atividade-Stack.ts
@@ -1,9 +1,58 @@
 // Pilha
 
 import { Stack } from "./Stack";
+import readlinesync = require("readline-sync");
 
-const pilha = new Stack<number>()
+const pilha = new Stack<string>()
+let opcao;
 
-pilha.push(5);
+        do {
 
-pilha.printStack();
+            console.log("\n-------------LIVRARIA TURMA 14-------------");
+            console.log(" 1 - Adicionar Livro na pilha ");
+            console.log(" 2 - Listar todos os livros ");
+            console.log(" 3 - retirar livro da pilha ");
+            console.log(" 0 - Sair ")   
+            console.log("-------------------------------------------");
+
+            opcao = readlinesync.questionInt("Entre com a opcao desejada: ");
+
+        switch(opcao){
+            case 1:
+                let nomedolivro = readlinesync.question("Digite o nome do livro que deseja adicionar: ");
+                pilha.push(nomedolivro);                
+                console.log("\nPilha:\n");
+                pilha.printStack();
+                console.log("\nLivro adicionado!");
+
+            break;
+
+            case 2:
+                console.log("Lista de Livros na Pilha: \n");
+                pilha.printStack();
+
+            break;
+
+            case 3:
+                if(pilha.isEmpty()){
+                    console.log("\nA Pilha está vazia!")
+                }else{
+                    pilha.pop();
+                    console.log("\nPilha atual:\n");
+                    pilha.printStack();
+                    console.log("\nUm Livro foi retirado da pilha!:\n");
+                }
+
+            break;
+
+            case 0:
+                console.log("\nPrograma Finalizado!");
+            break;
+
+            default:
+                console.log("Opcao invalida!")
+            break;
+        }
+            
+        
+        } while (opcao !== 0);
